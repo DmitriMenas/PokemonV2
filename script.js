8ea6ab75e9da649b0f98afccb1e9127496d6019c
DmitriMenas <152044184+DmitriMenas@users.noreply.github.com>
2024-04-09 17:01:25 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,24 @@ startImage.src = "./assets/pkmntitle.png";
 let worldMap = new Image();
 worldMap.src = "./assets/world.png";
 
+//player image
+let playerWidth = 46;
+let playerHeight = 46;
+let playerX = canvasWidth / 2 - playerWidth / 2;
+let playerY = canvasHeight / 2 - playerHeight;
+let playerRightImg;
+let playerLeftImg;
+let playerUpImg;
+let playerDownImg;
+
+let player = {
+    img: null,
+    x: playerX,
+    y: playerY,
+    width: playerWidth,
+    height: playerHeight,
+}
+
 
 window.onload = function() {
     canvas = document.getElementById("canvas");
@@ -20,22 +38,37 @@ window.onload = function() {
     context.fillStyle = "black";
     context.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    //draw title screen & enter game - using space bar
-    setTimeout(function() {
-        context.drawImage(startImage, 0, 0, canvas.width, canvas.height);
-    }, 1500);
-    context.font = "100px Arial";
-    context.fillStyle = "black";
-    context.fillText("Press Any Key to Start", canvasWidth / 2, canvasWidth / 2);
-    document.addEventListener("keydown", startGame)
+    // press any key to start text
+    context.font = "50px Arial";
+    context.fillStyle = "white";
+    context.fillText("Press Any Key to Start", canvasWidth* 1/6, canvasWidth / 2.5 );
+    document.addEventListener("keydown", startGame);
+    document.addEventListener("keydown", titleScreen);
+}
+
+function titleScreen() {
+    context.drawImage(startImage, 0, 0, canvas.width, canvas.height);
+    document.removeEventListener("keydown", titleScreen);
 }
 
 function startGame ()  {
-    document.removeEventListener("keydown", newGame);
-
+    document.removeEventListener("keydown", startGame);
     //draw world map
     setTimeout(function() {
-        context.drawImage(worldMap, 0, 0, canvas.width, canvas.height);
+    context.drawImage(worldMap, 0, 0, canvas.width, canvas.height);
     }, 2000);
     setInterval(update, 1000/60);
+
+    // Load player images
+    playerDownImg = new Image();
+    playerDownImg.src = "./assets/characterside-down.png";
+    player.img = playerDownImg;
+    playerDownImg.onload = function() {
+        // Once image is loaded, draw the player
+        context.drawImage(player.img, player.x, player.y, player.width, player.height);
+    };
+}
+
+function update() {
+
 }
